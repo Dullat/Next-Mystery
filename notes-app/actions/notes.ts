b0e7e5aa665e18bc26fa1500a2934aa1a9112be0
dullat <142017979+Dullat@users.noreply.github.com>
2026-08-28 00:26:23 +0530

    Feat: notes creation

--- notes-app/actions/notes.ts
+++ notes-app/actions/notes.ts
@@ -2,4 +2,31 @@
 
 import { auth } from "@/auth";
 import { prisma } from "@/lib/prisma"
-import { redirect } 
+import { redirect } from "next/navigation"
+import { revalidatePath } from "next/cache"
+
+export async function createNote(formData: FormData) {
+  const session = await auth();
+
+  if (!session?.user?.id) {
+    throw new Error("You must be logged in to create a Note")
+  }
+
+  const title = formData.get("title") as string
+  const content = formData.get("content") as string
+
+  if (!title || !content) {
+    throw new Error("Title and content is required")
+  }
+
+  await prisma.note.create({
+    data: {
+      title,
+      content,
+      authorId: session.user.id
+    }
+  })
+
+  revalidatePath("/dashboard")
+  redirect("/dashboard")
+}
